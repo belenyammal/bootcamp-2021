25d07621e907a25b70062daa17bb5819b0c61bc6
belen yammal <belentammal@gmail.com>
2021-11-03 00:08:49 -0300

    enunciado 1 terminado

---
+++ clase1/enunciado1/src/main/java/Curso.java
@@ -0,0 +1,94 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+@Getter
+@Setter
+public class Curso {
+    private String nombre;
+    private int cantHoras;
+    private ArrayList<Alumno> alumnos;
+
+    public Curso(String nombre, int cantHoras, ArrayList<Alumno> alumnos) {
+        this.nombre = nombre;
+        this.cantHoras = cantHoras;
+        this.alumnos = alumnos;
+    }
+
+    public void setAlumnos(Alumno a) {
+        this.alumnos.add(a);
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "nombre='" + nombre + '\'' +
+                ", cantHoras=" + cantHoras +
+                ", alumnos=" + alumnos +
+                '}';
+    }
+
+    public float calcularPromedioCurso() {
+        int sum = 0;
+        for (Alumno alumno: alumnos) {
+            sum += alumno.calcularPromedio();
+        }
+        return (float) sum / alumnos.size();
+    }
+
+    public ArrayList<Alumno> alumnosAprobados() {
+        ArrayList aprobados = new ArrayList<Alumno>();
+        for (Alumno alumno: alumnos) {
+            if (alumno.calcularPromedio() >= 6) {
+                aprobados.add(alumno);
+            }
+        }
+        return aprobados;
+    }
+
+    public void ordenarAlumnos() {
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                Collections.sort(alumnos, new Comparator() {
+
+                    public int compare(Object o1, Object o2) {
+                        Alumno p1 = (Alumno) o1;
+                        Alumno p2 = (Alumno) o2;
+                        int res =  p1.getApellido().compareToIgnoreCase(p2.getApellido());
+                        if (res != 0)
+                            return res;
+                        return p1.getNombre().compareToIgnoreCase(p2.getNombre());
+                    }
+                });
+            }
+
+        }
+
+
+        /*
+        ordena pero solo por apellido
+        int n = alumnos.length;
+        for(int i = 0; i < n - 1; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                int r = alumnos[i].compareTo(alumnos[j]);
+                if( r > 0)
+                {
+                    Alumno aux = alumnos[i];
+                    alumnos[i] = alumnos[j];
+                    alumnos[j] = aux;
+                }
+            }
+        }
+        */
+
+
+    }
+
+}
